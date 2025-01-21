bd507a3ebdf842922cf6f4d86d84e7d0b873eace
MarlineIOS <blanxxtygadress@gmail.com>
2025-01-21 16:07:20 +0300

    ADDED VALIDATION

--- VinylStore.Api/Program.cs
+++ VinylStore.Api/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using VinylStore.Api.Persistence;
+using FluentValidation.AspNetCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,7 @@ var connString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<VinylStoreContext>(options =>
     options.UseSqlServer(connString));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssembly(Assembly.Load("VinylStore.Shared")));
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
--- VinylStore.Shared/Features/ManageStore/AlbumDto.cs
+++ VinylStore.Shared/Features/ManageStore/AlbumDto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace VinylStore.Shared.Features.ManageStore
 {
@@ -22,6 +23,28 @@ namespace VinylStore.Shared.Features.ManageStore
         {
             public string Name { get; set; } = string.Empty;
             public string Text { get; set; } = string.Empty;
+        }
+    }
+
+    public class AlbumValidator: AbstractValidator<AlbumDto>
+    {
+        public AlbumValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Please enter a name");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Please enter a description");
+            RuleFor(x => x.AuthorName).NotEmpty().WithMessage("Please enter a author name");
+            RuleFor(x => x.TimeInMinutes).NotEmpty().WithMessage("Please enter a time in minutes");
+            RuleFor(x => x.Rating).NotEmpty().WithMessage("Please enter a rating");
+            RuleForEach(x => x.Songs).SetValidator(new SongValidator());
+        }
+    }
+
+    public class SongValidator: AbstractValidator<AlbumDto.Song>
+    {
+        public SongValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Please enter a name");
+            RuleFor(x => x.Text).NotEmpty().WithMessage("Please enter a text");
         }
     }
 }
